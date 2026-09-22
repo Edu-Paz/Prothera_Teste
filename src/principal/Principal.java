f06e025c4aae010f10bcf7baad3a334f522d920e
Edu-Paz <eduardopazcarv@gmail.com>
2026-09-21 22:03:46 -0300

    add methods to manage and display Funcionario instances in Principal class

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -3,15 +3,33 @@ package principal;
 import entities.Funcionario;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class Principal {
     public static void main(String[] args) {
         List<Funcionario> funcionarios = new ArrayList<>();
 
+        inserirFuncionarios(funcionarios);
+        System.out.println("Funcionários inseridos com sucesso");
+
+        removerFuncionario(funcionarios, "João");
+        System.out.println("Funcionário João removido com sucesso!");
+
+        imprimirFuncionarios(funcionarios);
+
+        aumentarSalario(funcionarios);
+
+        imprimirFuncionarios(funcionarios);
+
+    }
+
+    public static void inserirFuncionarios(List<Funcionario> funcionarios) {
         funcionarios.add(new Funcionario("Maria", LocalDate.of(2000, 10, 18), new BigDecimal("2009.44"), "Operador"));
         funcionarios.add(new Funcionario("João", LocalDate.of(1990, 5, 12), new BigDecimal("2284.38"), "Operador"));
         funcionarios.add(new Funcionario("Caio", LocalDate.of(1961, 5, 2), new BigDecimal("9836.14"), "Coordenador"));
@@ -23,17 +41,39 @@ public class Principal {
         funcionarios.add(new Funcionario("Heloísa", LocalDate.of(2003, 5, 24), new BigDecimal("1606.85"), "Eletricista"));
         funcionarios.add(new Funcionario("Helena", LocalDate.of(1996, 9, 2), new BigDecimal("2799.93"), "Gerente"));
 
-        funcionarios.removeIf(funcionario -> funcionario.getNome().equals("João"));
+    }
 
+    public static void removerFuncionario(List<Funcionario> funcionarios, String nome) {
+        funcionarios.removeIf(funcionario -> funcionario.getNome().equals(nome));
+
+    }
+
+    public static void imprimirFuncionarios(List<Funcionario> funcionarios) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.forLanguageTag("pt-BR"));
+        symbols.setDecimalSeparator(',');
+        symbols.setGroupingSeparator('.');
+
+        DecimalFormat df = new DecimalFormat("#,##0.00", symbols);
+
+        System.out.println("Lista de funcionários:");
 
         for (Funcionario funcionario : funcionarios) {
             System.out.println(
                     "Nome: " + funcionario.getNome()
                             + " | Data: " + funcionario.getDataNascimento().format(formatter)
-                            + " | Salário: " + funcionario.getSalario()
+                            + " | Salário: R$ " + df.format(funcionario.getSalario())
                             + " | Função: " + funcionario.getFuncao()
             );
+        }
+
+        System.out.println("***FIM***");
+        System.out.println();
+    }
+
+    public static void aumentarSalario(List<Funcionario> funcionarios){
+        for (Funcionario funcionario : funcionarios) {
+            funcionario.aumentarSalario(new BigDecimal("0.10"));
         }
     }
 }
